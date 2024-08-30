58c00f004e80c0ebb9ff28f5383aa6c6ba26616f
ritesh-doijad <riteshdoijad284@gmail.com>
2024-08-30 14:58:53 +0530

    download

--- src/components/PhotosPurchased.jsx
+++ src/components/PhotosPurchased.jsx
@@ -1,6 +1,13 @@
 import React, { useEffect, useState } from "react";
-import { collection, getDocs, query, where, deleteDoc, doc } from "firebase/firestore";
-import { firestore } from "../Firebase/Firebase";
+import {
+  collection,
+  getDocs,
+  query,
+  where,
+  deleteDoc,
+  doc,
+} from "firebase/firestore";
+import { firestore, storage } from "../Firebase/Firebase";
 import { useDispatch, useSelector } from "react-redux";
 import toast from "react-hot-toast";
 import { setMyPosts } from "../redux/Slices/postSlice";
@@ -8,6 +15,9 @@ import ImageCard from "./ImageCard";
 import { BiSolidMessageSquareEdit } from "react-icons/bi";
 import { MdDelete } from "react-icons/md";
 import DashBoardHeader from "./DashBoardHeader";
+import { IoArrowDownCircle } from "react-icons/io5";
+import { getDownloadURL, ref } from "firebase/storage";
+import { saveAs } from 'file-saver';
 
 const PhotosPurchased = () => {
   const user = useSelector((state) => state.auth.user);
@@ -22,13 +32,18 @@ const PhotosPurchased = () => {
       if (!userId || posts.length > 0) return;
       setLoading(true);
       const productsRef = collection(firestore, "posts");
-      const q = query(productsRef, where("purchasedBy", "array-contains", userId));
+      const q = query(
+        productsRef,
+        where("purchasedBy", "array-contains", userId)
+      );
       const querySnapshot = await getDocs(q);
 
       const postsData = querySnapshot.docs.map((doc) => ({
         id: doc.id,
         ...doc.data(),
-        createdAt: doc.data().createdAt ? doc.data().createdAt.toMillis() : null,
+        createdAt: doc.data().createdAt
+          ? doc.data().createdAt.toMillis()
+          : null,
       }));
       if (postsData.length === 0) {
         setPostsFound(false);
@@ -58,9 +73,23 @@ const PhotosPurchased = () => {
     if (posts.length === 0) {
       getMyPosts();
     } else {
-      setPostsFound(true); // If posts are already in state, set postsFound to true
+      setPostsFound(true);
     }
   }, [posts.length]);
+
+  const downloadImage = async (imageUrl) => {
+    try {
+      const storageRef = ref(storage, imageUrl);
+      const url = await getDownloadURL(storageRef);
+  
+      saveAs(url, imageUrl.substring(imageUrl.lastIndexOf('/') + 1));
+  
+      console.log('Image downloaded successfully!');
+    } catch (error) {
+      console.error('Error downloading image:', error);
+    }
+  };
+  
 
   return (
     <div>
@@ -68,8 +97,15 @@ const PhotosPurchased = () => {
       <h1 className="text-2xl font-semibold mb-5 ml-8">Image Purchased</h1>
       <div className="mx-8">
         {loading && <p>Loading...</p>}
-        {!loading && !postsFound && <p className="text-center text-lg">No images purchased.</p>}
-        <div className={`grid md:grid-cols-3 gap-4 overflow-y-auto scrollbar-hide ${postsFound ? "" : "hidden"}`} style={{ maxHeight: "550px" }}>
+        {!loading && !postsFound && (
+          <p className="text-center text-lg">No images purchased.</p>
+        )}
+        <div
+          className={`grid md:grid-cols-3 gap-4 overflow-y-auto scrollbar-hide ${
+            postsFound ? "" : "hidden"
+          }`}
+          style={{ maxHeight: "550px" }}
+        >
           {posts.map(({ id, title, price, userName, imageUrl }) => (
             <ImageCard
               key={id}
@@ -78,9 +114,10 @@ const PhotosPurchased = () => {
               title={title}
               price={price}
               icon1={
-                <BiSolidMessageSquareEdit
-                  title="Edit"
+                <IoArrowDownCircle
+                  title="Download Now"
                   className="text-2xl text-black cursor-pointer hover:scale-110 transition-all ease-linear duration-300"
+                  onClick={() => downloadImage(imageUrl)}
                 />
               }
               icon2={
